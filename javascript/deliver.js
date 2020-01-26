a1b14386c767ed3d6cef1e1d9da81f220faf2761
armetegithub <armomomete@protonmail.com>
2020-01-26 11:39:51 +0100

    Objeto deliver se coloca en el canvas rn posición aleatoria

---
+++ javascript/deliver.js
@@ -0,0 +1,22 @@
+class Deliver {
+
+    constructor(gameWidth, gameHeight) {
+
+        this.width = 30;
+        this.height = 30;
+
+        this.position = {
+
+            x: gameWidth * Math.random(),
+            y: gameHeight * Math.random()
+        }
+    }
+
+    draw(ctx) {
+
+        ctx.fillStyle = '#321';
+        ctx.fillRect(this.position.x, this.position.y, this.height, this.width);
+
+
+    }
+}
---
+++ javascript/game.js
@@ -0,0 +1,91 @@
+/** @type HTMLCanvasElement */
+/** @type CanvasRenderingContext2D */
+
+const game = {
+
+    canvas: undefined,
+    ctx: undefined,
+    width: undefined,
+    height: undefined,
+    FPS: 60,
+    time: undefined,
+    player: undefined,
+    packet: undefined,
+    obstacles: [],
+    keys: {
+
+        TOP: 38,
+        RIGHT: 39,
+        DOWN: 40,
+        LEFT: 37,
+    },
+
+    init() {
+
+        this.canvas = document.getElementById("canvas");
+        this.ctx = this.canvas.getContext('2d');
+        this.ctx = this.canvas.getContext("2d");
+        this.width = window.innerWidth * 0.98;
+        this.height = window.innerHeight * 0.98;
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.setDimensions();
+        this.drawAll();
+
+
+
+
+
+
+    },
+
+    start() {
+
+        this.interval = setInterval(() => {
+
+            this.drawAll();
+
+
+        }, 1000 / 60);
+    },
+
+
+    drawAll() {
+
+
+        this.draw();
+
+
+
+    },
+
+    draw() {
+
+        let deliver = new Deliver(this.width, this.height);
+        deliver.draw(this.ctx);
+
+
+    },
+
+
+    setDimensions() {
+
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.canvas.setAttribute("width", `${this.width}px`);
+        this.canvas.setAttribute("height", `${this.height}px`);
+
+
+
+    },
+
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+    },
+
+
+
+}
